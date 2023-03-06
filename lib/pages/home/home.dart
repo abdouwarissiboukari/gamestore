3682879f88ee3fc00aca28339e469bac47d7721b
Abdou Warissi Boukari <wariss7@gmail.com>
2023-03-06 10:37:37 +0000

    Commit n°1 from baw#mba-waris

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -13,7 +13,7 @@ class HomePage extends StatelessWidget {
           children: [
             Transform(
               transform: Matrix4.identity()..rotateZ(20),
-              origin: const Offset(150, 20),
+              origin: const Offset(150, 50),
               child: Image.asset(
                 'assets/images/bg_liquid.png',
                 width: 200,
@@ -46,7 +46,90 @@ class HomePage extends StatelessWidget {
   }
 
   Widget NavigationBar() {
-    return Container();
+    return Container(
+      color: const Color(0xfff6f8ff),
+      child: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 5,
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          child: BottomNavigationBar(
+            selectedItemColor: const Color(0xFF5F67EA),
+            selectedFontSize: 12,
+            unselectedFontSize: 12,
+            unselectedItemColor: Colors.grey.withOpacity(0.7),
+            type: BottomNavigationBarType.fixed,
+            items: [
+              const BottomNavigationBarItem(
+                label: 'Home',
+                icon: Icon(
+                  Icons.home_rounded,
+                  size: 50,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: 'Application',
+                icon: Container(
+                  margin: const EdgeInsets.all(5),
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Icon(
+                    Icons.more_horiz_outlined,
+                    size: 30,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: 'Film',
+                icon: Container(
+                  margin: const EdgeInsets.all(5),
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Icon(
+                    Icons.play_arrow_rounded,
+                    size: 30,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: 'Book',
+                icon: Container(
+                  margin: const EdgeInsets.all(5),
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Icon(
+                    Icons.auto_stories_rounded,
+                    size: 30,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   Widget HeaderSection() {
