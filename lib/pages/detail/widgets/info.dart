78c35e96b8a27ff49df00aae90013d42a00f0b7c
Abdou Warissi Boukari <wariss7@gmail.com>
2023-03-07 11:17:07 +0000

    Commit n°2 from baw#mba-waris

--- lib/pages/detail/widgets/info.dart
+++ lib/pages/detail/widgets/info.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:gamestore/models/game.dart';
+import 'package:gamestore/pages/detail/widgets/description.dart';
+import 'package:gamestore/pages/detail/widgets/gallery.dart';
+import 'package:gamestore/pages/detail/widgets/header.dart';
+import 'package:gamestore/pages/detail/widgets/review.dart';
 
 class GameInfo extends StatelessWidget {
   final Game game;
@@ -12,10 +16,10 @@ class GameInfo extends StatelessWidget {
       color: Colors.white,
       child: Column(
         children: [
-          // HeaderSection(game),
-          // GallerySection(game),
-          // DescriptionSection(game),
-          // ReviewSection(game),
+          HeaderSection(game),
+          GallerySection(game),
+          DescriptionSection(game),
+          ReviewSection(game),
         ],
       ),
     );
